9d92e34a848f66f8a2d57ddf940c39aa78bada6b
jbytow <jakub.bytow@gmail.com>
2023-08-03 11:07:53 +0200

    0.49 Meal forms WIP

--- gltracker/static/js/mealForm.js
+++ gltracker/static/js/mealForm.js
@@ -1,18 +1,17 @@
 $(document).ready(function() {
     $("#add-form").on("click", function(e) {
         e.preventDefault();
-        var extraFormsInput = $("#id_extra_forms");
-        var numExtraForms = parseInt(extraFormsInput.val(), 10);
         var formsetContainer = $("#formset-container");
         var totalForms = $("#id_form-TOTAL_FORMS").val();
 
-        for (var i = 0; i < numExtraForms; i++) {
-            var newFormHtml = formsetContainer.find(".mealitem-form:last").html();
-            newFormHtml = newFormHtml.replace(/__prefix__/g, totalForms);
-            formsetContainer.append('<div class="mealitem-form">' + newFormHtml + '</div>');
-            totalForms++;
-        }
+        var newFormHtml = formsetContainer.find(".mealitem-form:last").html();
+        newFormHtml = newFormHtml.replace(/__prefix__/g, totalForms);
+        formsetContainer.append('<div class="mealitem-form">' + newFormHtml + '</div>');
+        totalForms++;
+    });
 
-        extraFormsInput.val(1);  // Resetuj pole na 1
+    $(document).on("click", ".remove-form", function(e) {
+        e.preventDefault();
+        $(this).closest(".mealitem-form").remove();
     });
 });
