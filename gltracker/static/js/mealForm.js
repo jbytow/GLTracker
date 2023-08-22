f5a8910efc5bfd42be95040bc3d19ffad6840df0
jbytow <jakub.bytow@gmail.com>
2023-08-22 16:00:10 +0200

    0.61 foodDetails.js name change to mealDetails.js, minor html template change

--- gltracker/static/js/mealForm.js
+++ gltracker/static/js/mealForm.js
@@ -1,50 +1,22 @@
-//$(document).ready(function() {
-//    $("#add-form").on("click", function(e) {
-//        e.preventDefault();
-//        var extraFormsInput = $("#id_extra_forms");
-//        var numExtraForms = parseInt(extraFormsInput.val(), 10);
-//        var formsetContainer = $("#formset-container");
-//        var totalFormsInput = $("#id_form-TOTAL_FORMS");  // Pobierz pole form-TOTAL_FORMS
-//        var totalForms = parseInt(totalFormsInput.val(), 10);  // Konwertuj na liczbę
-//
-//        for (var i = 0; i < numExtraForms; i++) {
-//            var newFormHtml = formsetContainer.find(".mealitem-form:last").html();
-//            newFormHtml = newFormHtml.replace(/__prefix__/g, totalForms);
-//            formsetContainer.append('<div class="mealitem-form">' + newFormHtml + '</div>');
-//            totalForms++;  // Zwiększ liczbę formularzy
-//        }
-//
-//        totalFormsInput.val(totalForms);  // Aktualizuj pole form-TOTAL_FORMS
-//        extraFormsInput.val(1);  // Resetuj pole na 1
-//    });
-//});
+const addMoreBtn = document.getElementById('add-more');
+const totalNewForms = document.getElementById('id_form-TOTAL_FORMS')
 
+addMoreBtn.addEventListener('click', add_new_form);
 
-//const addMoreBtn = document.getElementById('add-more')
-//const totalNewForms = document.getElementById('id_form-TOTAL_FORMS')
-//
-//addMoreBtn.addEventListener('click', add_new_form)
-//
-//function add_new_form(args)
-
-
-
-//addMoreBtn.addEventListener('click', add_new_form)
-//function add_new_form(event) {
-//    if (event) {
-//    event.preventDefault()
-//    }
-//    const currentIngredientForms = document.getElementsByClassName('ingredient-form')
-//    const currentFormCount = CurrentIngredientForms.length // + 1
-//    const formCopyTarget = document.getElementById('ingredient-form-list')
-//    const emptyFormEl = document.getElementById('empty-form').cloneNode(true)
-//    copyEmptyFormEl.setAttribute('class', 'ingredient-form')
-//    copyEmptyFormEl.setAttribute('id', `form-${currentFormCount}`)
-//    const regex = new RegExp('__prefix__', 'g')
-//    copyEmptyFormEl.innerHTML = copyEmptyFormEl.innerHTML.replace(regex, currentFormCount)
-//    totalNewForms.setAttribute('value', currentFormCount + 1)
-//    // add new empty form element to html form
-//    formCopyTarget.append(emptyFormEl)
-//    }
+function add_new_form(event) {
+    if (event) {
+        event.preventDefault()
+    }
+    const currentIngredientForms = document.getElementsByClassName('ingredient-form')
+    const currentFormCount = currentIngredientForms.length
+    const formCopyTarget = document.getElementById('ingredient-form-list')
+    const copyEmptyFormEl = document.getElementById('empty-form').cloneNode(true)
+    copyEmptyFormEl.setAttribute('class', 'ingredient-form')
+    copyEmptyFormEl.setAttribute('id', `form-${currentFormCount}`)
+    const regex = new RegExp('__prefix__', 'g')
+    copyEmptyFormEl.innerHTML = copyEmptyFormEl.innerHTML.replace(regex, currentFormCount)
+    totalNewForms.setAttribute('value', currentFormCount + 1)
+    formCopyTarget.append(copyEmptyFormEl)
+}
 
 
