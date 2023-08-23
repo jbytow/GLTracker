69625549ea1017fd6a018b4b6765d54d782c0511
jbytow <jakub.bytow@gmail.com>
2023-08-23 11:30:59 +0200

    0.64 minor html template customization, old forms definitive cleanup

--- gltracker/food/forms.py
+++ gltracker/food/forms.py
@@ -15,30 +15,6 @@ class MealItemForm(forms.ModelForm):
 
 
 class MealForm(forms.ModelForm):
-    class Meta:
-        model = Meal
-        fields = ['name', 'image']
-
-    def __init__(self, user=None, *args, **kwargs):
-        super(MealForm, self).__init__(*args, **kwargs)
-        self.user = user
-
-    def save(self, commit=True):
-        meal = super(MealForm, self).save(commit=False)
-        if self.user:
-            meal.user = self.user
-        if commit:
-            meal.save()
-        return meal
-
-
-class RecipeIngredientForm(forms.ModelForm):
-    class Meta:
-        model = MealItem
-        fields = ['food_item', 'quantity']
-
-
-class RecipeForm(forms.ModelForm):
     error_css_class = 'error-field'
     required_css_class = 'required-field'
     name = forms.CharField(widget=forms.TextInput(attrs={"class": "form-control",
